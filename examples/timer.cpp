fdc5a0c51dab93ff21d1194f9fe2e31c2e77b910
Shivakar Vulli <svulli@shivakar.com>
2017-03-22 18:28:31 -0400

    Added cmake file structure and placeholder files.

---
+++ examples/timer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "timer.hpp"
+
+int main(int argc, char *argv[]) {
+
+    hello();
+
+    return 0;
+}
---
+++ src/timer.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "timer.hpp"
+
+void hello(void) { std::cout << "Hello, World!!" << std::endl; }
