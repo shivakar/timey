a3ca054d100bf3697e9f7ef372e79b0c17c640c8
Shivakar Vulli <svulli@shivakar.com>
2017-04-01 17:09:31 -0400

    Added function to convert duration to string.
    Added internal function to convert double to fixed format string without
    trailing zeros.
    
    Added tests for Humanize and DoubleToFixedString.

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,7 +1,81 @@
 #pragma once
 
+#include <iomanip>
 #include <chrono>
+#include <cstdint>
+#include <sstream>
 
 namespace timey {
-std::string Humanize(std::chrono::duration<double> dur) { return "hello"; }
+typedef std::chrono::duration<int64_t, std::nano> NanosecondsType;
+typedef std::chrono::duration<int64_t, std::micro> MicrosecondsType;
+typedef std::chrono::duration<int64_t, std::milli> MillisecondsType;
+typedef std::chrono::duration<int64_t> SecondsType;
+typedef std::chrono::duration<int64_t, std::ratio<60>> MinutesType;
+typedef std::chrono::duration<int64_t, std::ratio<60 * 60>> HoursType;
+
+constexpr NanosecondsType Nanosecond(1);
+constexpr MicrosecondsType Microsecond(1);
+constexpr MillisecondsType Millisecond(1);
+constexpr SecondsType Second(1);
+constexpr MinutesType Minute(1);
+constexpr HoursType Hour(1);
+constexpr SecondsType ZeroSeconds(0);
+
+namespace internal {
+std::string DoubleToFixedString(double d) {
+    std::ostringstream out;
+
+    out << std::fixed << std::setprecision(9) << d;
+    std::string astr = out.str();
+    std::size_t idx = astr.find_last_not_of("0");
+    if (astr[idx] != '.') {
+        idx += 1;
+    }
+    astr.erase(astr.begin() + idx, astr.end());
+
+    return astr;
+}
+}
+
+template <class T1, class T2>
+std::string Humanize(const std::chrono::duration<T1, T2>& dur) {
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+
+    std::ostringstream out;
+    int64_t i;
+    auto d = duration_cast<NanosecondsType>(dur);
+
+    if (d == ZeroSeconds) {
+        out << "0s";
+    } else if (d < Second) {
+        if (d >= Millisecond) {
+            out << (duration_cast<duration<double, std::milli>>(d)).count()
+                << "ms";
+        } else if (d >= Microsecond) {
+            out << (duration_cast<duration<double, std::micro>>(d)).count()
+                << "us";
+        } else {
+            out << (duration_cast<duration<double, std::nano>>(d)).count()
+                << "ns";
+        }
+    } else {
+        if (d >= Hour) {
+            i = (duration_cast<HoursType>(d)).count();
+            out << i << "h";
+            d -= i * Hour;
+        }
+        if (d >= Minute) {
+            i = (duration_cast<MinutesType>(d)).count();
+            out << i << "m";
+            d -= i * Minute;
+        }
+        if (d != ZeroSeconds) {
+            out << internal::DoubleToFixedString(
+                       (duration_cast<duration<double>>(d)).count()) << "s";
+        }
+    }
+
+    return out.str();
+}
 }
---
+++ test/utils_test.cpp
@@ -0,0 +1,53 @@
+#include <chrono>
+
+#include "gtest/gtest.h"
+
+#include "timey.hpp"
+
+TEST(TimeyInternalTest, DoubleToFixedString) {
+    std::vector<std::pair<double, std::string>> test_data = {
+        std::make_pair(0, "0"), std::make_pair(0.0, "0"),
+        std::make_pair(1.0, "1"), std::make_pair(2.01, "2.01"),
+        std::make_pair(12.000004, "12.000004"),
+        std::make_pair(123.000007, "123.000007"),
+        std::make_pair(1e-9, "0.000000001"),
+        std::make_pair(77580.000000001, "77580.000000001")
+        // End of test_data
+    };
+
+    for (auto test : test_data) {
+        EXPECT_EQ(timey::internal::DoubleToFixedString(test.first),
+                  test.second);
+    }
+}
+
+TEST(TimeyTest, HumanizeDuration) {
+    std::vector<std::pair<timey::NanosecondsType, std::string>> test_data = {
+        std::make_pair(0 * timey::Second, "0s"),
+        std::make_pair(1 * timey::Nanosecond, "1ns"),
+        std::make_pair(123 * timey::Nanosecond, "123ns"),
+        std::make_pair(32 * timey::Microsecond, "32us"),
+        std::make_pair(999 * timey::Microsecond, "999us"),
+        std::make_pair(1000 * timey::Microsecond, "1ms"),
+        std::make_pair(250 * timey::Millisecond, "250ms"),
+        std::make_pair(2500 * timey::Millisecond, "2.5s"),
+        std::make_pair(12 * timey::Second, "12s"),
+        std::make_pair(120 * timey::Second, "2m"),
+        std::make_pair(1 * timey::Minute + timey::Microsecond, "1m0.000001s"),
+        std::make_pair(
+            45 * timey::Minute + 12 * timey::Second + timey::Nanosecond,
+            "45m12.000000001s"),
+        std::make_pair(90 * timey::Minute, "1h30m"),
+        std::make_pair(100 * timey::Hour + 90 * timey::Minute +
+                           5 * timey::Second + 5 * timey::Millisecond +
+                           1 * timey::Microsecond + 4 * timey::Nanosecond,
+                       "101h30m5.005001004s"),
+        std::make_pair(((1UL << 63) - 1) * timey::Nanosecond,
+                       "2562047h47m16.854775807s")
+        // End of test_data
+    };
+
+    for (auto test : test_data) {
+        EXPECT_EQ(timey::Humanize(test.first), test.second);
+    }
+}
